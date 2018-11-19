2f2af5b64d78aaaa93d404ad6283d384f5da4302
ruby.jiro <cddpt903@yahoo.co.jp>
2018-11-19 13:32:29 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :microposts, only: [:create, :destroy]
+  
 end
